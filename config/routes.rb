031413cead1a5a8af9186486cbdd0189d1c201d5
tearoflily <tearoflily1019@gmail.com>
2020-04-05 13:31:30 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'kintaib_pages#top'
+  get '/signup', to:'users#new'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
